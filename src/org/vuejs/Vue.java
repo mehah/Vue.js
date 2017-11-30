0d8682183b714dcd730d94af19632dd4c0276145
Renato Machado <mehahx@gmail.com>
2017-11-30 12:04:32 -0200

    update

--- src/org/vuejs/Vue.java
+++ src/org/vuejs/Vue.java
@@ -1,6 +1,8 @@
 package org.vuejs;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.jrender.jscript.DOM;
@@ -10,68 +12,133 @@ import com.jrender.jscript.dom.Window;
 import com.jrender.kernel.JRenderContext;
 
 public class Vue extends DOM {
-	private final Map<String, Object> map = new HashMap<String, Object>();
-	
+	private final Map<String, Object> options = new HashMap<String, Object>();
 	private boolean initialized = false;
-	private String el;
-	
+
 	public Vue(Window window) {
 		super(window);
 	}
-	
+
 	public Vue() {
 		super(JRenderContext.getInstance().currentWindow());
 	}
-	
-	public void setEl(String query) {
-		if(this.initialized) {
+
+	public void el(String query) {
+		if (this.initialized) {
 			return;
 		}
-		
-		this.el = query;
-		this.map.put("el", el);
+
+		this.options.put("el", query);
+	}
+
+	public String el() {
+		return (String) this.options.get("el");
 	}
 	
-	public String getEl(String query) {
-		return this.el;
+	public void delimiters(String[] delimiters) {
+		if (this.initialized) {
+			return;
+		}
+
+		this.options.put("delimiters", delimiters);
 	}
-	
-	public void setData(String name, Object value) {
-		Map<String, Object> data = (Map<String, Object>) map.get("data");
-		
-		if(data == null) {
+
+	public String[] delimiters() {
+		return (String[]) this.options.get("delimiters");
+	}	
+
+	public void inject(List<String> list) {
+		if (this.initialized) {
+			return;
+		}
+
+		this.options.put("list", list);
+	}
+
+	public List<String> inject() {
+		return (List<String>) this.options.get("inject");
+	}
+
+	public List<Map<String, Object>> mixins() {
+		List<Map<String, Object>> mixins = (List<Map<String, Object>>) options.get("mixins");
+
+		if (mixins == null) {
+			mixins = new ArrayList<Map<String, Object>>();
+			this.options.put("mixins", mixins);
+		}
+
+		return mixins;
+	}
+
+	public void mixins(Map<String, Object> options) {
+		if (this.initialized) {
+			return;
+		}
+
+		mixins().add(options);
+	}
+
+	private Map<String, Object> __data() {
+		Map<String, Object> data = (Map<String, Object>) this.options.get("data");
+
+		if (data == null) {
 			data = new HashMap<String, Object>();
-			this.map.put("data", data);
+			this.options.put("data", data);
 		}
 		
-		data.put(name, value);
-		if(this.initialized) {
+		return data;
+	}
+	
+	public void data(String name, Object value) {
+		__data().put(name, value);
+		if (this.initialized) {
 			DOMHandle.setProperty(this, name, value);
 		}
 	}
-	
+
 	public Object getData(String name) {
-		Map<String, Object> data = (Map<String, Object>) map.get("data");
-		if(data != null) {
-			return data.get(name);
-		}
-		
-		return null;
+		return __data().get(name);
 	}
-	
+
 	public void registerMethod(String name) {
-		Map<String, FunctionHandle> methods = (Map<String, FunctionHandle>) map.get("methods");
-		
-		if(methods == null) {
+		Map<String, FunctionHandle> methods = (Map<String, FunctionHandle>) options.get("methods");
+
+		if (methods == null) {
 			methods = new HashMap<String, FunctionHandle>();
-			this.map.put("methods", methods);
+			this.options.put("methods", methods);
 		}
-		
+
 		methods.put(name, new FunctionHandle(name));
 	}
-	
+
+	public void registerComputed(String methodName) {
+		Map<String, FunctionHandle> methods = (Map<String, FunctionHandle>) options.get("computed");
+
+		if (methods == null) {
+			methods = new HashMap<String, FunctionHandle>();
+			this.options.put("computed", methods);
+		}
+
+		methods.put(methodName, new FunctionHandle(methodName));
+	}
+
+	public void registerWatch(String methodName) {
+		Map<String, FunctionHandle> methods = (Map<String, FunctionHandle>) options.get("watch");
+
+		if (methods == null) {
+			methods = new HashMap<String, FunctionHandle>();
+			this.options.put("watch", methods);
+		}
+
+		methods.put(methodName, new FunctionHandle(methodName));
+	}
+
+	public void onCreated(String methodName) {
+		this.options.put("created", new FunctionHandle(methodName));
+	}
+
 	public void init() {
 		this.initialized = true;
-		DOMHandle.newInstance(this, "Vue", this.map);
+		DOMHandle.newInstance(this, "Vue", this.options);
 	}
 }
